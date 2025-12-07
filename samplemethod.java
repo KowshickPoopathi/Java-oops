f7a1ee8d93061276c9f6a21a087e17d1fa96a6d4
KowshickPoopathi <kowshickpoopathi@gmail.com>
2025-12-07 22:55:33 +0530

    Method in java

---
+++ samplemethod.java
@@ -0,0 +1,13 @@
+//1
+
+public class samplemethod {
+    public static void main(String[] args) {
+        add();
+    }
+    public static void add(){
+        int a = 10 ;
+        int b = 20 ;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
