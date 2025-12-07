6f52e3ae03089bacdea83f3a3e131bdb7d58fe6a
KowshickPoopathi <kowshickpoopathi@gmail.com>
2025-12-07 22:58:03 +0530

    Non Void Method in java

---
+++ nonvoidsamplemethod.java
@@ -0,0 +1,18 @@
+//2
+
+public class nonvoidsamplemethod {
+    public static void main(String[] args) {
+    //int sample = add();
+    //System.out.println(sample);
+    System.out.println(add(10,20));
+    }
+    public static int add(int a , int b){
+        //int a = 10 ;
+       // int b = 20 ;
+        int c = a+b;
+        return c;
+    }
+}
+  
+
+// parameter value 
